cbf426fc09d073b0a2fbec6b768630d3a2555450
Pablo Camarillo <p.camarillor@gmail.com>
2025-01-26 21:47:47 -0600

    Removing old files

--- spark_cluster/run_kafka_producer.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-docker exec -it kafka_producer python3 /opt/spark-apps/$1 --kafka-bootstrap $2 --kafka-topic $3
-
--- spark_cluster/start_jupyter.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-docker run -d --name=jupyter-iteso --network spark_cluster_default \
---volumes-from spark_cluster-spark-master-1 \
--p 8888:8888 \
-spark-submit
