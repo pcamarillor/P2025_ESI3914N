1c4de86d853f7b6970392d2a2cba78c09fcf00ff
Pablo Camarillo <p.camarillor@gmail.com>
2025-02-19 15:44:30 -0600

    Adding resources for lecture 08

---
+++ spark_cluster/notebooks/lib/team_name/spark_utils.py
@@ -0,0 +1,6 @@
+from pyspark.sql.types import StructType
+
+class SparkUtils:
+    @staticmethod
+    def generate_schema(columns_info) -> StructType:
+        raise NotImplementedError("Not implemented yet")
