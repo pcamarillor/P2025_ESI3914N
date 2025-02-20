d9d639200ea014f0335417cdf7612ff4584482e4
KonradSch98 <konrado.schindler@t-online.de>
2025-02-19 19:22:11 -0600

    first_attempt_generate_schema_dict_listcompr

--- spark_cluster/notebooks/lib/foraneos/spark_utils.py
+++ spark_cluster/notebooks/lib/foraneos/spark_utils.py
@@ -1,6 +1,26 @@
-from pyspark.sql.types import StructType
+from pyspark.sql.types import StructField, StructType, StringType, DoubleType, IntegerType, FloatType, BooleanType, ShortType,LongType, MapType, ArrayType, TimestampType ,DateType
 
 class SparkUtils:
     @staticmethod
     def generate_schema(columns_info) -> StructType:
-        raise NotImplementedError("Not implemented yet")
+    
+        TypeDict = {
+            "string": StringType(),
+            "double": DoubleType(),
+            "int": IntegerType(),
+            "float": FloatType(),
+            "bool": BooleanType(), 
+            "data": DateType(),
+            "timestamp": TimestampType(),
+            "array": ArrayType(),
+            "map": MapType(),
+            "struct": StructType(), 
+            "long": LongType(),
+            "short": ShortType()
+        }
+        
+
+        schema_list = [ StructField((tuple_arg(0), TypeDict[tuple_arg(1)], True)) for tuple_arg in columns_info ]
+    
+    
+        return StructType(schema_list)
