8565d32a80c68fee4133965edaf3e62a8b9442b1
diegorozcos <diegorozcos122@gmail.com>
2025-02-20 21:42:34 -0600

    finished lab04 team par de dos

--- spark_cluster/notebooks/lib/team_ParDeDos/spark_utils.py
+++ spark_cluster/notebooks/lib/team_ParDeDos/spark_utils.py
@@ -1,8 +1,9 @@
 from pyspark.sql.types import StructType, StructField, StringType, IntegerType, LongType, ShortType, DoubleType, FloatType, BooleanType, DateType, TimestampType, BinaryType, ArrayType, MapType
 
 class SparkUtils:
-    def __init__(self):
-        self.typeDict = {
+    @staticmethod
+    def generate_schema(columns_info) -> StructType:
+        typeDict = {
             "StringType": StringType(),
             "IntegerType": IntegerType(),
             "LongType": LongType(),
@@ -16,17 +17,8 @@ class SparkUtils:
             "MapType": MapType(StringType(), StringType()),  # Se requieren clave y valor
             "DateType": DateType(),
         }
-
-    # [("name", "StringType"),
-    #  ("age", "IntegerType"),
-    #  ("city", "StringType")]
-
-    @staticmethod
-    def generate_schema(self, columns_info) -> StructType:
-        # list of tuples
         schema = []
         for k, v in columns_info:
-            if v in self.typeDict:
-            # StructField("name", StringType(), True),
-                schema.append(StructField(f"${k}", type.get(v), True))
+            if v in typeDict:
+                schema.append(StructField(f"${k}", typeDict.get(v), True))
         return schema
