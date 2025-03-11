179c5f969f0e2466dd79783a5bc77e5a2b982dda
AaronOrtega1 <aaron.ortega15@outlook.es>
2025-03-10 19:40:44 -0600

    Lab 06 par de dos uncompleted

--- spark_cluster/notebooks/lib/team_ParDeDos/spark_utils.py
+++ spark_cluster/notebooks/lib/team_ParDeDos/spark_utils.py
@@ -1,6 +1,62 @@
 from pyspark.sql.types import StructType, StructField, StringType, IntegerType, LongType, ShortType, DoubleType, FloatType, BooleanType, DateType, TimestampType, BinaryType, ArrayType, MapType
 
 class SparkUtils:
+
+    @staticmethod
+    def clean_df(df, schema):
+        """_summary_
+
+        Args:
+            df (_type_): _description_
+
+        Returns:
+            _type_: _description_
+        """
+
+        """
+        ("show_id", "string"),
+        ("type", "string"),
+        ("title", "string"),
+        ("director", "string"),
+        ("country", "string"),
+        ("rating", "string"),
+        ("duration", "string"),
+        ("listed_in", "string"),
+        ("release_year", "integer"),
+        ("date_added", "date")
+        """
+        defaults = {
+            "string": "hola",
+            "integer": 15,
+            "long": 100,
+            "short": 12,
+            "double": 123.1,
+            "float": 123.123,
+            "boolean": True,
+            "date": "lol",
+            "timestamp": "lol",
+            "binary": "lol"
+        }
+
+
+        for k, v in schema:
+            if v not in defaults:
+                raise TypeError(f"{v} is not a valid type.")
+            temp_df = df.fill(value=defaults.get(v),subset=[f"{k}"])
+        return temp_df
+
+    @staticmethod
+    def write_df(df):
+        """_summary_
+
+        Args:
+            df (_type_): _description_
+
+        Returns:
+            _type_: _description_
+        """
+        return 0
+
     @staticmethod
     def generate_schema(columns_info) -> StructType:
         """_summary_
