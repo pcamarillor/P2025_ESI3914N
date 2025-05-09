ee5c28a0857a384883fd6f070c5a3357e550765b
KonradSch98 <konrado.schindler@t-online.de>
2025-05-09 13:11:50 -0600

    first_finish_producer_utils

--- spark_cluster/notebooks/lib/foraneos/utils_proyectofinal.py
+++ spark_cluster/notebooks/lib/foraneos/utils_proyectofinal.py
@@ -75,23 +75,13 @@ class SparkUtils:
         return df.fillna(correction_dict)
         
             
-            
-            
-            
-      4 producers 
-4 topics 
-
-how often logging
-how many messages per logging
-which information type of logging
-which information structure       
-            
+        
             
             
             
 class Producers:
     
-    def __init__(self, ticker: string, kafka_server: string, kafka_topic: string, log_time: float = 2.0, start_date: string = "2025-01-01", end_date: str = None):
+    def __init__(self, ticker: string, kafka_server: string, kafka_topic: string, publ_interval: float = 2.0, start_date: string = "2025-01-01", end_date: str = None):
         '''
         This class creates random log files from a server where the log messages contains
             date time , Topic , info1 , info2 , info3
@@ -99,38 +89,39 @@ class Producers:
         And the user can specify how many log messages per log file should be written. Not set is 10 seconds. 
         
         Args:
-            log_time (int, optional):               int or 'random', interval between log messages, default is 2 seconds
-            log_file_number (int, optional):        number of log messages pero log file, default is random 
-            log_topic (string):                     which topic to stream ()
+            publ_interval (int, optional):               int or 'random', interval between log messages, default is 2 seconds
+
         Returns:
             
         '''
         
-        
-        self.run_logs       = False       #bool for active logging      
-        self.log_time       = log_time
-        self.ticker         = ticker            
+        self.run_producer   = False         #bool for active logging      
+        self.publ_interval  = publ_interval      #how often to publish to kafka
+        self.ticker         = ticker        #id of stock
         self.kafka_topic    = kafka_topic 
         self.kafka_server   = kafka_server
-        self.start_date     = start_date 
-        self.end_date       = end_date   
-        self.hist           = None
-        self.price_iterator = None
-        self.last           = None
+        self.start_date     = start_date    #for stock download
+        self.end_date       = end_date      #for stock download
+        self.hist           = None          #downloaded history of stock
+        self.price_iterator = None          #iterator for calculated prices
+        self.last           = None          #last close price from previously simulated prices
+        
         
         
     def __get_historical_data(self):
         """
-        Downloads historical close prices for a given ticker using yfinance.
+            Downloads historical close prices for a given ticker using yfinance.
 
-        Args:
-            ticker (str): Stock ticker symbol (e.g., "AAPL").
-            start_date (str): Start date in "YYYY-MM-DD" format.
-            end_date (str): End date in "YYYY-MM-DD" format. Defaults to today.
+            Args:
+                ticker (str): Stock ticker symbol (e.g., "AAPL").
+                start_date (str): Start date in "YYYY-MM-DD" format.
+                end_date (str): End date in "YYYY-MM-DD" format. Defaults to today.
 
-        Returns:
-            pd.DataFrame: DataFrame with "Close" prices.
+            Returns:
+                pd.DataFrame: DataFrame with "Close" prices.
         """
+        
+        
         if self.end_date is None:
             self.end_date = datetime.today().strftime("%Y-%m-%d")
 
@@ -139,22 +130,23 @@ class Producers:
     
     
     
-    def __simulate_gbm_prices(self,r, sigma, n_periods, period_seconds):
+    def __simulate_gbm_prices(self, r, sigma, n_periods, period_seconds):
         """
-        Simulates stock prices using the Geometric Brownian Motion (GBM) model.
+            Simulates stock prices using the Geometric Brownian Motion (GBM) model.
 
-        Args:
-            S0 (float):             Initial stock price.
-            r (float):              Risk-free interest rate (annualized).
-            sigma (float):          Volatility of the stock (annualized).
-            n_periods (int):        Number of time steps to simulate.
-            period_seconds (int):   Time window between each simulated price in seconds.
+            Args:
+                
+                r (float):              Risk-free interest rate (annualized).
+                sigma (float):          Volatility of the stock (annualized).
+                n_periods (int):        Number of time steps to simulate.
+                period_seconds (int):   Time window between each simulated price in seconds.
 
-        Returns:
-            List[float]: Simulated stock prices including the initial price.
+            Returns:
+                List[float]: Simulated stock prices including the initial price.
         """
+        
         dt = period_seconds / (365 * 24 * 60 * 60)
-        prices = [self.last]
+        prices = [self.last]        #init start price
 
         for _ in range(n_periods):
             z = np.random.normal()
@@ -165,8 +157,12 @@ class Producers:
     
     
     
+    
+    
     def __Download_Stock_Hist(self):
-        
+        '''
+            init download of stock price history
+        '''
         
         try:
             self.hist = self.__get_historical_data()
@@ -180,19 +176,21 @@ class Producers:
             
             
         
+        
+
     def __simulate_prices(self, window_seconds=10, n_periods=50, r=0.01, sigma=0.2):
         """
-        Simulates future stock prices using GBM for each ticker.
+            Simulates future stock prices using GBM for each ticker.
 
-        Args:
-            
-            window_seconds (int):       Time window in seconds between each simulated price.
-            n_periods (int):            Number of future prices to simulate.
-            r (float):                  Risk-free interest rate (annualized).
-            sigma (float):              Volatility (annualized).
+            Args:
+                
+                window_seconds (int):       Time window in seconds between each simulated price.
+                n_periods (int):            Number of future prices to simulate.
+                r (float):                  Risk-free interest rate (annualized).
+                sigma (float):              Volatility (annualized).
 
-        Returns:
-            pd.DataFrame:               DataFrame with timestamps as index and ticker columns.
+            Returns:
+                pd.DataFrame:               DataFrame with timestamps as index and ticker columns.
         """
         
 
@@ -207,48 +205,42 @@ class Producers:
             
         return df_prices
 
+
+
+
     
     def __resample_and_aggregate(self, df ,new_window):
         """
-        produce prices on interval of 5secs
-        calculate ohcl every 5mins
-        so in interval of 60/5*5 = 60 elements
-        
-        
-        Processes simulated prices into OHLC format
+           
+            Processes simulated prices into OHLC format
 
-        Args:
-            df (pd.DataFrame): DataFrame with timestamps as index and ticker symbols as columns.
-            new_window (str): Pandas-compatible resampling window (e.g., '5min', '15min').
+            Args:
+                df (pd.DataFrame):  DataFrame with timestamps as index and ticker symbols as columns.
+                new_window (str):   Pandas-compatible resampling window (e.g., '5min', '15min').
 
-        Returns:
-            List[pd.DataFrame]: List of dataframes (one per ticker) with:
-                - OHLC prices
-                - 4 technical indicators: RSI, Williams %R, Ultimate Oscillator, EMA
-                - 5 lagged close prices
+            Returns:
+                List[pd.DataFrame]: List of dataframes (one per ticker) with:
+                    - OHLC prices
         """
         
         
-        result = []
-        
         # Create OHLC DataFrame directly from price data
-        ohlc_df = df.copy()
+        sim_prices_df = df.copy()
         
         # Force float type to prevent dtype=object errors
-        ohlc_df['price'] = pd.to_numeric(ohlc_df['price'], errors='coerce')
-        
-        ohlc_df = ohlc_df.dropna()
+        sim_prices_df['price'] = pd.to_numeric(sim_prices_df['price'], errors='coerce')
+        sim_prices_df = ohlc_df.dropna()
     
         # Ensure timestamp index is datetime
-        ohlc_df.index = pd.to_datetime(ohlc_df.index)
+        sim_prices_df.index = pd.to_datetime(sim_prices_df.index)
         
         ohlc_df = pd.DataFrame()
         
         # Resample to get OHLC
-        ohlc_df['open']     = ohlc_df['price'].resample(new_window).first()
-        ohlc_df['high']     = ohlc_df['price'].resample(new_window).max()
-        ohlc_df['low']      = ohlc_df['price'].resample(new_window).min()
-        ohlc_df['close']    = ohlc_df['price'].resample(new_window).last()
+        ohlc_df['open']     = sim_prices_df['price'].resample(new_window).first()
+        ohlc_df['high']     = sim_prices_df['price'].resample(new_window).max()
+        ohlc_df['low']      = sim_prices_df['price'].resample(new_window).min()
+        ohlc_df['close']    = sim_prices_df['price'].resample(new_window).last()
         ohlc_df             = ohlc_df.dropna()
         
         if ohlc_df.empty:
@@ -256,11 +248,8 @@ class Producers:
             exit()
 
         ohlc_df = ohlc_df.dropna()
-        ohlc_df['ticker'] = self.ticker
-        
-        result.append(ohlc_df)
 
-        return result
+        return ohlc_df
 
 
 
@@ -268,7 +257,7 @@ class Producers:
     
     def __init_producer(self):
         '''
-        inits a kafka producer on a given server with a txt serializer
+            inits a kafka producer on a given server with a txt serializer
         '''
         
         try:
@@ -290,21 +279,20 @@ class Producers:
         
     def __start_producer(self):
         '''
-        Starts a loop that sends logging data to a kafka topic at a specific interval.
+            Starts a loop that sends logging data to a kafka topic at a specific interval.
         '''
         
+        #creates 50 prices in 10sec intervals with random interest rate and votality 
+        close_price_window      = 5                             #interval between artificial close prices
+        full_price_window       = 5                             #resmaple interval  for all prices in mins
+        resample                = string(full_price_window) + 'min'
+        #creates 20 price intervals
+        n_prices                = 60/close_price_window * full_price_window  * 20   #number of prices to create
+        self.last = self.hist["Close"].iloc[-1, 0]               #safe last price in history as next initial price
+        log_time_logger         =   datetime.now()
         
-        self.last = self.hist["Close"].iloc[-1, 0]  
-        while self.run_logs:
+        while self.run_producer:
             
-            #creates 50 prices in 10sec intervals with random interest rate and votality 
-            close_price_window      = 5                             #interval between artificial close prices
-            full_price_window       = 5                             #resmaple interval  for all prices in mins
-            resample                = string(full_price_window) + 'min'
-            #creates 20 price intervals
-            n_prices                = 60/close_price_window * full_price_window  * 20   #number of prices to create
-            
-
             #get next prices, or produce new prices if exhausted
             try:
                 current_prices = next(price_iterator)
@@ -313,13 +301,20 @@ class Producers:
                 next_prices_trajectory          = self.__resample_and_aggregate(next_close_price_trajectory, resample)
                 next_close_price_trajectory.set_index('name', inplace=True)
                 price_iterator                  = iter(next_prices_trajectory.iloc())
-                self.last                       = next_prices_trajectory["Close"].iloc[-1, 0]  
+                self.last                       = next_prices_trajectory["Close"].iloc[-1, 0]   #safe last simulated price as next init price
+            
             
             log_data   =  "{},{},{},{},{},{}".format(current_prices['timestamps'], self.ticker, 
                                     current_prices['open'], current_prices['high'], current_prices['low'], current_prices['close'])
             
+            
+            #wait time if faster than log interval
+            log_timediffer  = datetime.now() - log_time_logger
+            if  log_timediffer < self.publ_interval:
+                time.sleep(self.publ_interval - log_timediffer)
+                
             self.k_producer.send(self.kafka_topic, log_data)
-            time.sleep(self.log_time)
+            log_time_logger =   datetime.now()
     
     
 
@@ -330,7 +325,7 @@ class Producers:
         '''
             start the logging loop
         '''
-        self.run_logs = True
+        self.run_producer = True
         self.__init_producer()
         self.__start_producer(log_file_path)
         
@@ -341,24 +336,5 @@ class Producers:
         '''
             stop the logging loop
         '''
-        self.run_logs = False
+        self.run_producer = False
 
-
-
-
-
-
-class TrafficListener(StreamingQueryListener):
-    def onQueryStarted(self, event):
-        print(f"Query started: {event.id}")
-
-    def onQueryProgress(self, event):
-        num_input_rows = event.progress.numInputRows
-        print(f"Query made progress: {event.progress}")
-        
-        if num_input_rows >= 50:
-            send_alert(f"High volume of data: {num_input_rows} rows")
-
-
-    def onQueryTerminated(self, event):
-        print(f"Query terminated: {event.id}")
